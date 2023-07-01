514376cab45b0b97a0dc67b63fc384cf2475b2c1
Baris Balli <barisbll.dev@gmail.com>
2023-07-01 15:50:31 +0200

    Exercise 10: Strings are completed

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,21 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    let text = input.trim();
+    println!("text {}", text);
+    String::from(text)
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut yy = String::from(input);
+    yy.push_str(" world!");
+    yy
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut s = String::from(input.replace("cars", "balloons"));
+    s
 }
 
 #[cfg(test)]
