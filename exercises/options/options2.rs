efb64dd69a8fde56317c2fe52cc779bc0db3e1ba
Baris Balli <barisbll.dev@gmail.com>
2023-07-12 14:39:01 +0300

    Exercise 14: Options are completed

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
@@ -13,7 +11,7 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -28,13 +26,17 @@ mod tests {
         }
 
         let mut cursor = range;
+        let cursor_ref = &cursor;
 
         // TODO: make this a while let statement - remember that vector.pop also
         // adds another layer of Option<T>. You can stack `Option<T>`s into
         // while let and if let.
-        integer = optional_integers.pop() {
-            assert_eq!(integer, cursor);
-            cursor -= 1;
+
+        while let Some(i) = optional_integers.pop() { 
+            if let Some(k) = i {
+                assert_eq!(k, cursor);
+                cursor -= 1;
+            }
         }
 
         assert_eq!(cursor, 0);
