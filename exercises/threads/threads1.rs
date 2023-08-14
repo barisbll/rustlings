79034eb4a66e0b48ced7163f42844ce191a2a8cb
Baris Balli <barisbll.dev@gmail.com>
2023-08-14 08:56:14 +0300

    Exercise 21: Threads are completed

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -7,28 +7,30 @@
 //
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
-
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
 
 fn main() {
     let mut handles = vec![];
+    let results_vec = vec![];
+    let arc = Arc::new(Mutex::new(results_vec));
     for i in 0..10 {
+        let arc = Arc::clone(&arc);
         handles.push(thread::spawn(move || {
             let start = Instant::now();
             thread::sleep(Duration::from_millis(250));
             println!("thread {} is complete", i);
-            start.elapsed().as_millis()
+            let time_since = start.elapsed().as_millis();
+            let mut vector = arc.lock().unwrap();
+            vector.push(time_since);
         }));
     }
 
-    let mut results: Vec<u128> = vec![];
     for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+        handle.join().unwrap();
     }
-
+    let mut results = &*(arc.lock().unwrap());
     if results.len() != 10 {
         panic!("Oh no! All the spawned threads did not finish!");
     }
